329f0e642c1c1c940055d56180a0a7d75dbee434
Roman Konoplev <roman@konoplev.pro>
2019-04-19 14:26:30 +0300

    Add start, end and intermediate stations

--- lesson_03/route.rb
+++ lesson_03/route.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
 class Route
-  attr_reader :stations
-
   def initialize(start_station, end_station)
-    @stations = [start_station, end_station]
+    @start_station = start_station
+    @end_station = end_station
+
+    @intermediate_stations = []
   end
 
   def add_station(station)
-    @stations.insert(-2, station) unless [@stations.first, @stations.last].include?(station)
+    @intermediate_stations.push(station)
   end
 
   def delete_station(station)
-    @stations.delete(station) unless [@stations.first, @stations.last].include?(station)
+    @intermediate_stations.delete(station)
+  end
+
+  def stations
+    [@start_station, *@intermediate_stations, @end_station]
   end
 end
