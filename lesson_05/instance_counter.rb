4b8466053f98222abe0c4e91ee53b11f581d41f6
Roman Konoplev <roman@konoplev.pro>
2019-04-24 22:23:28 +0300

    Add default value for count instances of the class

--- lesson_05/instance_counter.rb
+++ lesson_05/instance_counter.rb
@@ -4,17 +4,22 @@ module InstanceCounter
   def self.included(klass)
     klass.extend ClassMethods
     klass.send :include, InstanceMethods
+
+    klass.instances = 0
   end
 
   module ClassMethods
     attr_accessor :instances
+
+    def inherited(subclass)
+      subclass.instances = 0
+    end
   end
 
   module InstanceMethods
     protected
 
     def register_instance
-      self.class.instances ||= 0
       self.class.instances += 1
     end
   end
